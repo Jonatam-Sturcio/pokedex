27453c2c2da230e766ba8e850b421bb0937f0407
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-24 15:44:42 -0300

    Implementação da API de pokemon, pegando apenas os nomes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PokemonApiService } from './services/pokemon-api.service';
+import { Pokemon } from './models/pokemon';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'pokedex';
+export class AppComponent implements OnInit {
+  public pokemons: Pokemon[];
+
+  constructor(private PokeApiService: PokemonApiService) {
+    this.pokemons = [];
+  }
+
+  ngOnInit(): void {
+    this.PokeApiService.selecionarTodos().subscribe((res) => {
+      const arrayResultados = res.results as any[];
+
+      this.pokemons = arrayResultados.map((obj) => {
+        return {
+          nome: this.converterParaTitleCase(obj.name),
+        };
+      });
+    });
+  }
+
+  private converterParaTitleCase(texto: string): string {
+    if (texto.length < 1) return texto;
+
+    return (
+      texto[0].toUpperCase() + texto.substring(1, texto.length).toLowerCase()
+    );
+  }
 }
---
+++ src/app/util/converter-para-title-case.ts
@@ -0,0 +1,7 @@
+function converterParaTitleCase(texto: string): string {
+  if (texto.length < 1) return texto;
+
+  return (
+    texto[0].toUpperCase() + texto.substring(1, texto.length).toLowerCase()
+  );
+}
